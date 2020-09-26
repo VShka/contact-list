e3004ad3c7ac8639f440570dbb51a164e6721984
VShka <viktor.shkarupa@yandex.ru>
2020-09-26 20:21:19 +0300

    brought detailed information about a contact into a separate module; 2) Add style for DetailAboutContact

---
+++ src/components/DetailAboutContact.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="form__container">
+    <h2>Детальная информация контакта: {{ detailContact.name }}</h2>
+    <form class="form-detail" @submit.prevent="getNewDetail">
+      <p>
+        Имя: {{ detailContact.name }}
+      </p>
+      <input
+        type="text"
+        name="name"
+        placeholder="Введите имя"
+        v-model="detailContact.name"
+      >
+      <p>
+        Фамилия: {{ detailContact.surname }}
+      </p>
+      <input
+        type="text"
+        name="surname"
+        placeholder="Введите фамилию"
+        v-model="detailContact.surname"
+      >
+      <p>
+        Email: {{ detailContact.email }}
+      </p>
+      <input
+        type="email"
+        name="email"
+        placeholder="Введите email"
+        v-model="detailContact.email"
+      >
+      <p>
+       Мобильный: {{ detailContact.tel }}
+      </p>
+      <input
+        type="text"
+        name="tel"
+        placeholder="Введите номер телефона"
+        v-model="detailContact.tel"
+      >
+
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from 'vuex';
+export default {
+  methods: {
+    ...mapMutations(['updateContacts']),
+
+    getNewDetail() {
+      this.updateContacts({
+        name: this.detailContact.name,
+        surname: this.detailContact.surname,
+        email: this.detailContact.email,
+        tel: this.detailContact.tel
+      })
+    }
+  },
+  computed: mapGetters(['detailContact'])
+}
+</script>
+
+<style lang="scss" scoped>
+
+.form__container {
+  margin-bottom: 1rem;
+
+  .form-detail {
+    max-width: 30%;
+    margin: 0 auto;
+    padding-bottom: 3rem;
+    border: 2px solid #ccc;
+
+    &:hover {
+        box-shadow: 0 0 10px rgb(141, 141, 138);
+      }
+
+    p {
+      font-weight: bold;
+      font-size: 24px;
+    }
+
+    input {
+      max-width: 50%;
+      padding: .5rem 2rem;
+      border: 1px solid rgb(7, 7, 155);
+      border-radius: 50px;
+
+      &:focus {
+        outline: none;
+        box-shadow: 0 0 10px rgb(141, 141, 138);
+      }
+    }
+  }
+}
+
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,71 +1,19 @@
 <template>
   <section class="about">
-    <h2>Детальная информация контакта: {{ detailContact.name }}</h2>
-    <form class="detail-info" @submit.prevent="getNewDetail">
-      <p> {{ detailContact.name }} </p>
-      <input
-        type="text"
-        name="name"
-        placeholder="Введите имя"
-        v-model="detailContact.name"
-      >
-      <p> {{ detailContact.surname }} </p>
-      <input
-        type="text"
-        name="surname"
-        placeholder="Введите фамилию"
-        v-model="detailContact.surname"
-      >
-      <p> {{ detailContact.email }} </p>
-      <input
-        type="email"
-        name="email"
-        placeholder="Введите email"
-        v-model="detailContact.email"
-      >
-      <p> {{ detailContact.tel }} </p>
-      <input
-        type="text"
-        name="tel"
-        placeholder="Введите номер телефона"
-        v-model="detailContact.tel"
-      >
-
-      <button
-        class="detail-info__button"
-        type="submit"
-      >
-        Сохранить изменения
-      </button>
-    </form>
+    <DetailAboutContact />
     <router-link to="/" class="link">На страницу контактов</router-link>
   </section>
 </template>
 
 <script>
-import { mapGetters, mapMutations } from 'vuex';
+import DetailAboutContact from '../components/DetailAboutContact';
 export default {
-  methods: {
-    ...mapMutations(['updateContacts']),
-
-    getNewDetail() {
-      this.updateContacts({
-        name: this.detailContact.name,
-        surname: this.detailContact.surname,
-        email: this.detailContact.email,
-        tel: this.detailContact.tel
-      })
-    }
-  },
-  computed: mapGetters(['detailContact'])
+  components: {
+    DetailAboutContact
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
-.detail-info {
-  display: flex;
-  flex-direction: column;
-}
 
 </style>
