aa32e37de908fdd8c56470f21bae511771c92076
VShka <viktor.shkarupa@yandex.ru>
2020-09-27 20:33:40 +0300

    rebuild template and add 2 button and style for them

--- src/components/DetailAboutContact.vue
+++ src/components/DetailAboutContact.vue
@@ -1,37 +1,54 @@
 <template>
   <div class="form__container">
     <h2>Детальная информация контакта: {{ detailContact.name }}</h2>
+
+    <div class="form-detail__info">
+      <p>
+        Имя:
+        <strong>
+          {{ detailContact.name }}
+        </strong>
+      </p>
+      <p>
+        Фамилия: 
+        <strong>
+          {{ detailContact.surname }}
+        </strong>
+      </p>
+      <p>
+        Email:
+        <strong>
+          {{ detailContact.email }}
+        </strong>
+      </p>
+      <p>
+        Мобильный:
+        <strong>
+          {{ detailContact.tel }}
+        </strong>
+      </p>
+    </div>
+
     <form class="form-detail" @submit.prevent="getNewDetail">
-      <p>
-        Имя: {{ detailContact.name }}
-      </p>
+      
       <input
-        type="text"
-        name="name"
-        placeholder="Введите имя"
-        v-model="detailContact.name"
+      type="text"
+      name="name"
+      placeholder="Введите имя"
+      v-model="detailContact.name"
       >
-      <p>
-        Фамилия: {{ detailContact.surname }}
-      </p>
       <input
         type="text"
         name="surname"
         placeholder="Введите фамилию"
         v-model="detailContact.surname"
       >
-      <p>
-        Email: {{ detailContact.email }}
-      </p>
       <input
         type="email"
         name="email"
         placeholder="Введите email"
         v-model="detailContact.email"
       >
-      <p>
-       Мобильный: {{ detailContact.tel }}
-      </p>
       <input
         type="text"
         name="tel"
@@ -39,6 +56,16 @@
         v-model="detailContact.tel"
       >
 
+      <div class="button-container">
+        <button class="button button_prev">
+          Отменить
+        </button>
+        
+        <button class="button button_save">
+          Сохранить
+        </button>
+      </div>
+      
     </form>
   </div>
 </template>
@@ -67,30 +94,69 @@ export default {
 .form__container {
   margin-bottom: 1rem;
 
+  p {
+      font-size: 24px;
+    }
+
   .form-detail {
-    max-width: 30%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    max-width: 25%;
+    min-height: 300px;
     margin: 0 auto;
-    padding-bottom: 3rem;
+    padding: 3rem 0;
     border: 2px solid #ccc;
 
     &:hover {
         box-shadow: 0 0 10px rgb(141, 141, 138);
       }
 
-    p {
-      font-weight: bold;
-      font-size: 24px;
-    }
-
     input {
-      max-width: 50%;
-      padding: .5rem 2rem;
-      border: 1px solid rgb(7, 7, 155);
+      width: 50%;
+      margin: 0 auto;
+      padding: 0.5rem 1rem;
+      border: 1px solid #07079b;
       border-radius: 50px;
 
       &:focus {
         outline: none;
         box-shadow: 0 0 10px rgb(141, 141, 138);
+      }
+    }
+
+    .button-container {
+      display: flex;
+
+      .button {
+        width: 30%;
+        margin: 0 auto;
+        padding: 0.5rem 1rem;
+        color: #fff;
+        font-weight: bold;
+        border-radius: 50px;
+        cursor: pointer;
+
+        &:focus {
+          outline: none;
+          box-shadow: 0 0 10px rgba(13, 19, 16, 0.7);
+        }
+      }
+      .button_save {
+        background: rgb(11, 160, 36);
+        border: 1px solid green;
+
+        &:active {
+          background: rgba(11, 160, 36, .7);
+        }
+      }
+      .button_prev {
+        background: red;
+        border: 1px solid red;
+
+        &:active {
+          background: rgba(202, 37, 37, 1);
+        }
       }
     }
   }
